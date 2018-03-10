8329ecea52d2b8c70c2b705f898fad2a1cec6436
thu <mingfei.net@gmail.com>
2018-03-10 15:23:15 +0800

    Update

--- node-svg/app.js
+++ node-svg/app.js
@@ -4,10 +4,10 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 
 let app = new express();
-app.use(bodyParser.urlencoded({extended:false}));
-app.use('/static', express.static('/public'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static('public'));
 
-app.post('/', (req, res)=>{
+app.post('/', (req, res) => {
     let name = req.body.name;
     let tel = req.body.tel;
     let email = req.body.email;
@@ -28,8 +28,11 @@ END:VCARD`;
         type: 'svg'
     });
     let time = new Date().getTime();
-    svg.pipe(fs.createWriteStream(`public/${time}.svg`));
-    res.redirect(`http://127.0.0.1:3000/public/${time}.svg`);
+    let stream = svg.pipe(fs.createWriteStream(`public/${time}.svg`));
+
+    stream.on('finish', () => {
+        res.redirect(`${time}.svg`);
+    });
 });
 
 app.listen(3000);
